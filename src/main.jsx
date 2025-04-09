c07be1773a867a63e2391e6a9b8785320f221f29
Bishoy Melek <bishoymelekwadie@gmail.com>
2025-04-09 19:17:51 +0200

    add react query

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,29 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './views/App';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 import ThemeProvider from './utils/ThemeContext';
+import App from './views/App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+// Create a query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // don't refetch when window focuses
+      staleTime: 5 * 60 * 1000, // 5 minutes
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </Router>
+    </QueryClientProvider>
   </React.StrictMode>
 );
